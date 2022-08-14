bd4f9149ff2013daec452fdc9f0ed34fb8e58900
amin <amincxo@gmail.com>
2022-08-14 21:01:48 +0430

    preState in hooks tamrin

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,12 +3,24 @@ import React , {useState} from 'react'
 const Counter = () => {
 
     const [number , setNumber] = useState(0);
+    const [name , setName] = useState('amin');
+
+
+    const nameHandler = () => {
+        if (name === "amin" ) {
+            setName("ali")
+        } else {
+            setName("amin")
+        }
+    }
     const clickHandler = () => {
-        setNumber(number+1)
+        setNumber(prevNumber => prevNumber + 1 )
     }
 
   return (
     <div>
+        <h1> {name} </h1>
+        <button onClick={nameHandler} > esmo avaz kon </button>
         <h1> {number} </h1>
         <button onClick={clickHandler} > Ziyad kon </button>
         {/* inline function on button */}
