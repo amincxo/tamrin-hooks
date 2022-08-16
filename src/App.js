ec7e14cbb525c4a14760aa072f3e92078e100a1a
amin <amincxo@gmail.com>
2022-08-16 13:29:45 +0430

    useefect in react hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React  from "react"
-import Form from "./components/Form";
+import Logger from "./components/Logger";
 
 function App() {
   return (
     <div >
-        <Form />
+        <Logger />
     </div>
   );
 }
---
+++ src/components/Logger.js
@@ -0,0 +1,20 @@
+import React , {useState , useEffect} from 'react';
+
+const Logger = () => {
+    const [number , setNumber] = useState(0);
+    const [second , setSecond] = useState(0);
+
+    useEffect(()=>{
+        console.log(number)
+    }, [number])
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={() => setNumber(prevNumber => prevNumber +1)} >up 1 </button>
+            <h1>{second}</h1>
+            <button onClick={() => setSecond(prevSecond => prevSecond +1)} >up second </button>
+        </div>
+    );
+};
+
+export default Logger;
