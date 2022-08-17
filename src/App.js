0e8011224673269b36540b7f15896150b0ffac8b
amin <amincxo@gmail.com>
2022-08-17 20:19:23 +0430

    Learn conText in Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React  from "react"
-import Data from "./components/Data"
+import React ,{useState , createContext}  from "react"
+import ComponentA from "./components/ComponentA";
+
+export const NameContext = createContext()
 
 const App = () => {
     
+    const [name, setname] = useState('aminamaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+
     return (
         <div>
-
-             <Data />
+             <NameContext.Provider value={name} >
+                <ComponentA />
+             </NameContext.Provider>
         </div>
     );
 };
