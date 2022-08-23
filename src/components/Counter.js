bffa60cad54c84765eb47980edb7014717ee4a22
amin <amincxo@gmail.com>
2022-08-23 12:33:09 +0430

    use two const in one useReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -22,14 +22,23 @@ const reducer = (state, action) => {
 const Counter = () => {
     
     const [number , dispach] = useReducer(reducer,initialState);
+    const [number2 , dispach2] = useReducer(reducer,initialState);
 
     return (
+        <>
         <div>
-            <h1>{number}</h1>
+            <h1>dispach 1: {number}</h1>
             <button onClick={() => dispach("Up")}>Up</button>
             <button onClick={() => dispach("Reset")}>Reset</button>
             <button onClick={() => dispach("Down")}>Down</button>
         </div>
+        <div>
+            <h1>dispach 2: {number2}</h1>
+            <button onClick={() => dispach2("Up")}>Up</button>
+            <button onClick={() => dispach2("Reset")}>Reset</button>
+            <button onClick={() => dispach2("Down")}>Down</button>
+        </div>
+        </>
     );
 };
 
